43069d07ecf21b0a852a0a9cc55616be6cd50c9a
RavenFoxx <53256536+RavenFoxx@users.noreply.github.com>
2022-01-05 18:54:04 +0800

    a

--- PGGE week 1 camera movement/Assets/Scripts/PlayerMovement.cs
+++ PGGE week 1 camera movement/Assets/Scripts/PlayerMovement.cs
@@ -15,14 +15,16 @@ public class PlayerMovement : MonoBehaviour
         float horiInp = Input.GetAxis("Horizontal");
         float vertiInp = Input.GetAxis("Vertical");
         float speed = this.mSpeed;
+
         if (Input.GetKey(KeyCode.LeftShift)) { speed = this.mSpeed * 2.0f; }
         if (playerAnim == null) return;
+
         transform.Rotate(0.0f, horiInp * rotSpeed * Time.deltaTime, 0.0f);
         Vector3 forward = transform.TransformDirection(Vector3.forward).normalized;
         forward.y = 0.0f;
         playerCC.Move(forward * vertiInp * speed * Time.deltaTime);
+
         playerAnim.SetFloat("PosX", 0);
-        playerAnim.SetFloat("PosZ", vertiInp * speed / 2.0f * this.mSpeed);
+        playerAnim.SetFloat("PosZ", vertiInp * speed / (2.0f * this.mSpeed));
     }
 }
-//test
